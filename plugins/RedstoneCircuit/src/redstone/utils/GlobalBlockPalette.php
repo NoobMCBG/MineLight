6a234263d7cd734d35f60ef5b330cc662ecc4889
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2022-01-25 09:49:55 +0700

    Add files via upload

---
+++ plugins/RedstoneCircuit/src/redstone/utils/GlobalBlockPalette.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace redstone\utils;
+
+use pocketmine\block\Block;
+
+class GlobalBlockPalette {
+
+    private $nameTable = [];
+    private $idTable = [];
+
+    public function __construct() {
+        $legacyIdMap = json_decode(file_get_contents(\pocketmine\RESOURCE_PATH . "vanilla/block_id_map.json"), true);
+        foreach($legacyIdMap as $name => $id){
+            $id = $legacyIdMap[$name];
+            $this->nameTable[$name] = $id;
+            $this->idTable[$id] = $name;
+        }
+    }
+
+    public function getId(string $name) : int {
+        return $this->nameTable[$name];
+    }
+
+    public function getBlock(string $name, int $damage) : Block {
+        return Block::get($this->getId($name), $damage);
+    }
+
+    public function getName(int $id) : string {
+        return $this->idTable[$id];
+    }
+
+    public function getNameAt(Block $block) : string {
+        return $this->idTable[$block->getId()];
+    }
+}
